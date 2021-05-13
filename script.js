8404dca6dc53029ecc9bd42b1b52c2645c424988
paul <paul.renobato@gmail.com>
2021-05-12 22:28:58 -0500

    Added the Alert function for when the user clicks on the generate password button it ask the length of the generated password.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var enterNumeric;
+Numeric = [1,2,3,4,5,6,7,8,9];
+
+var enterLetters;
+Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+
+
+
+
 // Write password to the #password input
+var user;
 function writePassword() {
+  user = parseInt(prompt("how much letters will be in the generated password?"))
+}if (user){alert(enter < 8 || enter > 128)
+  {
+  enterNumeric = confirm("will this password have numbers?");
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
