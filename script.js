4f1ae77d072f3bbb1d1cccb6fd24db5810d7393d
paul <paul.renobato@gmail.com>
2021-05-15 21:35:44 -0500

    implemented the different choices based upon the prompts you choose when selecting the type of password you want to generate

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ var enterLetters;
 Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 var enterUppercase;
-uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+Uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 var enterSpecial; 
 Special =  ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
+var input;
 // Write password to the #password input
 function writePassword() {
   enterLength = parseInt(prompt("How longs would you like your password? Between 8 and 128 Please."));
 
-
-  
   if (enterLength <= 8 || enterLength >= 128);
 
 else {
@@ -28,6 +27,54 @@ enterLetters = confirm("Will this contain lower case letters");
 enterUppercase = confirm("Will this contain upper case letters");
 enterSpecial = confirm("Will this contain special letters");
 };
+if (enterNumeric && enterLetters && enterUppercase && enterSpecial){
+  input = Numeric.concat(Letters, Uppercase, Special);
+}
+else if (enterNumeric && enterLetters && enterUppercase) {
+  input = Numeric.concat(Letters, Uppercase);
+}
+else if (enterNumeric && enterLetters && enterSpecial) {
+  input = Numeric.concat(Letters, Special);
+}
+else if (enterNumeric && enterUppercase && enterSpecial) {
+  input = Numeric.concat(Uppercase, Special);
+}
+else if (enterLetters && enterUppercase && enterSpecial) {
+  input = Letters.concat(Uppercase, Special);
+}
+else if (enterNumeric && enterLetters) {
+  input = Numeric.concat(Letters);
+
+} else if (enterNumeric && enterUppercase) {
+  input = Numeric.concat(Uppercase);
+
+} else if (enterNumeric && enterSpecial) {
+  input = Numeric.concat(Special);
+}
+else if (enterLetters && enterUppercase) {
+  input = Letters.concat(Uppercase);
+
+} else if (enterSpecial && enterLetters) {
+  input = Special.concat(Letters);
+
+} else if (enterUppercase && enterSpecial) {
+  input = Uppercase.concat(Special);
+}   
+else if (enterNumeric) {
+  input = Numeric;
+}
+else if (enterLetters) {
+  input = Letters;
+}
+else if (enterUppercase) {
+  input = Uppercase;
+}
+else if (enterSpecial) {
+  input = Special;
+}
+
+
+
 
 
 var password = writePassword();
